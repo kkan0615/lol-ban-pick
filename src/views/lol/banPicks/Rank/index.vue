4d723c1bbbc5651f976e80bba14819f673e59b5f
kkan0615 <kkan0615@gmail.com>
2021-06-01 23:19:05 +0900

    :rocket: version 0.0.1

--- src/views/lol/banPicks/Rank/index.vue
+++ src/views/lol/banPicks/Rank/index.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>
+  <div
+    class="w-full h-full"
+  >
     RankBankPickLol
+    <user-champion />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import UserChampion from '@/components/lol/UserChampion.vue'
 
 export default defineComponent({
   name: 'RankBankPickLol',
+  components: { UserChampion },
   setup () {
     return
   }
